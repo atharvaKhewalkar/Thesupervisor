0ec80304ec59662b12a7c803563742ca513bce3c
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2023-10-12 12:51:30 +0530

    Create account and login done

--- app.py
+++ app.py
@@ -29,14 +29,26 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-    users = mongo.db.users
-    login_user = users.find_one({'email': request.form['email']})
-
+    user_type = request.form.get('user_type')  # Get the selected user type
+    
+    if user_type == 'student':
+        collection = mongo.db.users
+    elif user_type == 'teacher':
+        collection = mongo.db.teachers
+    else:
+        return 'Invalid user type selected'
+    
+    login_user = collection.find_one({'email': request.form['email']})
+    
     if login_user:
         if request.form['password'] == login_user['password']:
-            return redirect(url_for('home'))
+            if user_type == 'student':
+                return redirect(url_for('home'))
+            elif user_type == 'teacher':
+                return redirect(url_for('teacherDashoard'))
     
     return 'Invalid email or password combination'
+
 
 @app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
@@ -44,21 +56,45 @@ def signUp():
         users = mongo.db.users
         existing_user = users.find_one({'email': request.form['email']})
         
-
         if existing_user is None:
             new_user = {
                 'name': request.form['username'],
                 'email': request.form['email'],
-                'password': request.form['password']
+                'password': request.form['password'],
+                'year': request.form['year'],
+                'department': request.form['department'],
+                'rollno': request.form['rollno']
             }
-            users.insert_one(new_user)  # Use insert_one here
+            users.insert_one(new_user)
             return redirect(url_for('index'))
+    
+    return 'User with this email already exists.'
 
+@app.route('/signUpTeacher', methods=['POST', 'GET'])
+def signUpTeacher():
+    if request.method == 'POST':
+        teachers = mongo.db.teachers
+        existing_user = teachers.find_one({'email': request.form['email']})
+        
+        if existing_user is None:
+            new_teacher = {
+                'name': request.form['username'],
+                'email': request.form['email'],
+                'password': request.form['password'],
+                'department': request.form['department']
+            }
+            teachers.insert_one(new_teacher)
+            return redirect(url_for('home'))
+    
     return 'User with this email already exists.'
 
 @app.route("/teacherLogin")
 def teacherLogin():
     return render_template('teacherlogin.html')
+
+@app.route("/teacherDashoard")
+def teacherDashoard():
+    return render_template('teacherDashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
