4bb8a4759b187c9d71fb816a9704b17fb82ae04f
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2023-12-14 18:49:18 +0530

    mail virification updated

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 
 # Set a secret key
-app.secret_key = 'ADBMS2023'  # Replace with a unique and secret key
+app.secret_key = 'IT2023'  # Replace with a unique and secret key
 
 # Rest of your code
-app.config['MONGO_DBNAME'] = 'ADBMS'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/ADBMS'
+app.config['MONGO_DBNAME'] = 'IT'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/IT'
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'  # Replace with your email server details
 app.config['MAIL_PORT'] = 587  # Replace with your email server port
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'riteshphadtare32@gmail.com'  # Replace with your email username
-app.config['MAIL_PASSWORD'] = 'pexc sczh kjik exbv'  # Replace with your email password
+app.config['MAIL_PASSWORD'] = 'ahvq xbdz jrcw wfyc'  # Replace with your email password
 
 mail = Mail(app)
 mongo = PyMongo(app)
